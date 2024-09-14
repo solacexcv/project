8a693114ed92909e636d7abeb4724d4c4af6ba7c
solacexcv <143187408+solacexcv@users.noreply.github.com>
2024-09-14 14:04:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ document.addEventListener('DOMContentLoaded', function () {
     const menuLinks = document.querySelector('.menu-links');
 
     menuIcon.addEventListener('click', function () {
+        if (menuLinks.classList.contains('show')) {
+            menuLinks.style.maxHeight = '0'; // Close with animation
+        } else {
+            menuLinks.style.maxHeight = menuLinks.scrollHeight + 'px'; // Open with animation
+        }
         menuLinks.classList.toggle('show');
     });
 });
